fc9758e6e4be6f414dec2ba4d62f3a14da54f3b8
Jose Algorta <jpalgort@gmail.com>
2021-02-09 18:47:20 -0300

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <Link to='/'>Go to <code>Home</code></Link>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React, Docker and Travis
+        </a>
+          <div>
+            <Route exact path='/' component={Fib} />
+            <Route exact path='/otherpage' component={OtherPage} />
+          </div>
+        </header>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const OtherPage = () => {
+    return <div>
+        I'm some other page!
+        <Link to='/'>Go to home!</Link>
+    </div>
+}
+
+export default OtherPage
