003ad38a96834e64fd30985afc845e8f80d8198b
Jose Algorta <jpalgort@gmail.com>
2021-02-10 00:26:36 -0300

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+docker build -t jopealgorta/multi-client:latest \
+    -t jopealgorta/multi-client:$SHA \
+    -f ./client/Dockerfile ./client
+
+docker build -t jopealgorta/multi-server:latest \
+    -t jopealgorta/multi-server:$SHA \
+    -f ./server/Dockerfile ./server
+
+docker build -t jopealgorta/multi-worker:latest \
+    -t jopealgorta/multi-worker:$SHA \
+    -f ./worker/Dockerfile ./worker
+
+docker push jopealgorta/multi-client:latest
+docker push jopealgorta/multi-server:latest
+docker push jopealgorta/multi-worker:latest
+
+docker push jopealgorta/multi-client:$SHA
+docker push jopealgorta/multi-server:$SHA
+docker push jopealgorta/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=jopealgorta/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jopealgorta/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jopealgorta/multi-worker:$SHA
